4850529a15944372db828e31b809af21c91eb9ce
Fellipe Castro <contact@fellipecastro.com>
2014-04-18 17:02:37 -0300

    Implementa bubble no menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-$( document ).ready(function() {    var pathname = window.location.pathname;    console.log( pathname );});
+$( document ).ready(function() {    var pathname = window.location.pathname;    console.log( pathname );    $("#menu > li > a").on("click", function(){        $("#menu > li").removeClass('active');        $(this).parent().addClass('active');        return false;    });});
